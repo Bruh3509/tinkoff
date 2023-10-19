758478cacb477520f8c0b09027764a0f14ae7361
Bruh3509 <iutrushkin@bk.ru>
2023-10-19 22:17:47 +0300

    first try

--- src/test/java/edu/project1/Project1Test.java
+++ src/test/java/edu/project1/Project1Test.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.ArrayList;
 
 public class Project1Test {
     private static final String ANSWER = "smth";
