b4e15da70ad9cb01b77d7b690e91434c09d18777
Bruh3509 <iutrushkin@bk.ru>
2023-11-14 21:55:45 +0300

    task1-task2

---
+++ src/main/java/edu/hw6/Task2.java
@@ -0,0 +1,48 @@
+package edu.hw6;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+
+public class Task2 {
+    public static String cloneFile(Path path) throws IOException {
+        var fileDirectory = path.getParent();
+        var fileName = path.getName(path.getNameCount() - 1).toString().split("\\.");
+        var name = fileName[0];
+        var extension = fileName[1];
+        var pattern = Pattern.compile(name + "( - copy( \\((\\d+)\\))?)\\." + extension);
+
+        var copyString = copyCalculation(fileDirectory, pattern);
+
+        var newFilePathName = Path.of(fileDirectory.toString(), name + copyString + "." + extension);
+        Files.copy(path, newFilePathName);
+        return name + copyString + extension;
+    }
+
+    private static String copyCalculation(Path fileDirectory, Pattern pattern) {
+        int maxCopy = 0;
+        var copyString = " - copy (1)";
+
+        try (var stream = Files.newDirectoryStream(fileDirectory)) {
+            for (var file : stream) {
+                var matcher = pattern.matcher(file.getName(file.getNameCount() - 1).toString());
+                if (matcher.find() && matcher.group(3) != null) {
+                    var copyCount = Integer.parseInt(matcher.group(3));
+                    if (copyCount > maxCopy) {
+                        maxCopy = copyCount;
+                        copyString = " - copy (" + (++copyCount) + ")";
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return copyString;
+    }
+
+    private Task2() {
+    }
+}
---
+++ src/test/java/edu/hw6/Task2Test.java
@@ -0,0 +1,13 @@
+package edu.hw6;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Task2Test {
+    @Test
+    @DisplayName("Test Task2")
+    void testTask2() throws IOException { // TODO testing
+    }
+}
