13890ae1037437b60fa28dcf64fbbe9149cba5e0
Bruh3509 <iutrushkin@bk.ru>
2023-12-16 19:22:30 +0300

    first version

--- src/main/java/edu/project5/ReflectionBenchmark.java
+++ src/main/java/edu/project5/ReflectionBenchmark.java
@@ -1,8 +1,13 @@
 package edu.project5;
 
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
@@ -29,7 +34,7 @@ public class ReflectionBenchmark {
             .warmupIterations(1)
             .warmupTime(TimeValue.seconds(5))
             .measurementIterations(1)
-            .measurementTime(TimeValue.seconds(5))
+            .measurementTime(TimeValue.seconds(30))
             .build();
 
         new Runner(options).run();
@@ -40,16 +45,53 @@ public class ReflectionBenchmark {
 
     private Student student;
     private Method method;
+    private MethodHandle handler;
+    private Supplier<String> lambda;
 
     @Setup
-    public void setup() throws NoSuchMethodException {
+    public void setup()
+        throws Throwable {
         student = new Student("Ivan", "Utrushkin");
-        method = ReflectionBenchmark.class.getMethod("directAccess", Blackhole.class);
+
+        method = ReflectionBenchmark.Student.class.getMethod("name");
+
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodType type = MethodType.methodType(String.class);
+        handler = lookup.findVirtual(Student.class, "surname", type);
+
+        var site = LambdaMetafactory.metafactory(
+            lookup,
+            "get",
+            MethodType.methodType(Supplier.class, Student.class),
+            MethodType.methodType(Object.class),
+            lookup.unreflect(method),
+            MethodType.methodType(String.class)
+        );
+
+        lambda = (Supplier<String>) site.getTarget().bindTo(student).invoke();
     }
 
     @Benchmark
     public void directAccess(Blackhole blackhole) {
         String name = student.name();
+        blackhole.consume(name);
+    }
+
+    @Benchmark
+    public void reflection(Blackhole blackhole) throws InvocationTargetException, IllegalAccessException {
+        String name = (String) method.invoke(student);
+        blackhole.consume(name);
+    }
+
+    @Benchmark
+    public void methodHandles(Blackhole blackhole) throws Throwable {
+        String surname = (String) handler.invoke(student);
+        blackhole.consume(surname);
+    }
+
+    @Benchmark
+    public void lambdaMeta(Blackhole blackhole) throws Throwable {
+        String name = lambda.get();
         blackhole.consume(name);
     }
 }
